d7fffde402a6b06c833d709391a510da36a51ac3
iwu01 <iwu01@students.cs.ubc.ca>
2022-09-03 21:49:34 -0700

    updated api

--- recipes/api/urls.py
+++ recipes/api/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
   path('', views.getRoutes),
   path('recipes/', views.getRecipes),
   path('tools/', views.getTools),
+<<<<<<< HEAD
   path('ingredient/', views.getIngredients),
   path('ingredientAmount/', views.getIngredientAmount),
   path('instruction/', views.getInstruction),
+=======
+>>>>>>> 179c0b8... api tested
 ]
